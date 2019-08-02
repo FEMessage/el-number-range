21fc802232937c348831b8648ad468096ea0c391
ColMugX <colmugx@qq.com>
2019-08-02 17:20:52 +0800

    feature: 添加 blur, focus 监听 (#31)

--- src/el-number-range.vue
+++ src/el-number-range.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="el-number-range">
-    <el-input :style="{width}" type="number" v-model.number="minValue" @blur="clampMin" />
+    <el-input
+      :style="{width}"
+      type="number"
+      v-model.number="minValue"
+      @blur="clampMin($event)"
+      @focus="handleFocus($event, 'min')"
+    />
     <span class="separator">{{ separator }}</span>
-    <el-input :style="{width}" type="number" v-model.number="maxValue" @blur="clampMax" />
+    <el-input
+      :style="{width}"
+      type="number"
+      v-model.number="maxValue"
+      @blur="clampMax($event)"
+      @focus="handleFocus($event, 'max')"
+    />
   </div>
 </template>
 <script>
@@ -67,17 +79,22 @@ export default {
     }
   },
   methods: {
-    clampMin() {
+    clampMin(event) {
+      this.$emit('blur', event, 'min')
       if (this.minValue === undefined) return
       let val = this.clamp(this.minValue)
       if (this.maxValue !== undefined) val = Math.min(val, this.maxValue)
       this.$emit('input', [val, this.maxValue])
     },
-    clampMax() {
+    clampMax(event) {
+      this.$emit('blur', event, 'max')
       if (this.maxValue === undefined) return
       let val = this.clamp(this.maxValue)
       if (this.minValue !== undefined) val = Math.max(val, this.minValue)
       this.$emit('input', [this.minValue, val])
+    },
+    handleFocus(event, position) {
+      this.$emit('focus', event, position)
     },
     clamp(val) {
       return Math.max(this.min, Math.min(this.max, val))
